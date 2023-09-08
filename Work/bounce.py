61d0e40a9bb5ca07019816a42f48ea6bc4bf95ef
OT-11 <135372462+OT-11@users.noreply.github.com>
2023-09-08 11:50:01 -0400

    Update bounce.py, first attempts

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,16 @@
 # bounce.py
 #
 # Exercise 1.5
+# ATTEMPT 1
+#--------------------------
+# Define Variables
+height = 100 #in metres
+bounce_factor = 3/5
+# Make Table Framework
+print("Bounce #\tHeight (m)")
+print("--------\t-----------")
+# Create loop
+for bounce in range (1, 11): #11 to be 10 inclusive 
+  height *= bounce_factor #*= is shorthand for height = height * bounce factor
+  print(f"{bounce}\t\t{height:.2f}") #2.f specifies decimals
+  
